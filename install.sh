28ad239bd9ec71c493763a701548866b7ec00736
ludovicplt <ludovic.peltier@epitech.eu>
2022-01-13 21:15:34 +0400

    push

--- install.sh
+++ install.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 
+path=${PWD}
+
 cd /tmp
 git clone "git@github.com:Epitech/dump.git"
 cd dump
 chmod +x "install_packages_dump.sh"
 ./install_packages_dump.sh
+cd "${path}"
 sudo dnf install "i3-gaps" "rofi" "polybar" "picom" "dialog"
-cp ./brightness /usr/bin/brightness
-cp ./my_boot /usr/bin/my_boot
+cp ./brightness /usr/bin/
+cp ./my_boot /usr/bin/
+cp -r picom ~/.config/
+cp -r i3 ~/.config/
 git clone "git@github.com:Heliferepo/suckless.git"
 cd suckless
-make install
+install.sh
